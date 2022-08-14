b121d22c894daf3652f6346d0289347e840cd028
Yuto2000 <urushimayuto@gmail.com>
2022-08-14 22:09:27 +0900

    ABC081B - Shift only

---
+++ 0814/step1.go
@@ -0,0 +1,50 @@
+// ABC081B - Shift only
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	n := scan()
+	var x []int
+	var counter int
+	var scounter int
+
+	for i:=0; i<n; i++{
+		x = append(x, scan())
+	}
+
+	eternal:
+	for {
+		for i, v := range x {
+			if a := v%2; a==0 {
+				x[i] = v/2
+				counter++
+			} else {
+				break eternal
+			}
+		}
+		if counter == len(x) {
+			scounter++
+		}
+		counter = 0
+	}
+	fmt.Println(scounter)
+}
+
+func scan() int {
+	sc.Scan()
+	i, err := strconv.Atoi(sc.Text())
+	if err != nil {
+		os.Exit(1)
+	}
+	return i
+}
