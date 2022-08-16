7e63c40bb87e833ebc5c188701367d1334a0f79f
Yuto2000 <urushimayuto@gmail.com>
2022-08-16 23:58:00 +0900

    ABC085B - Kagami Mochi

--- 0816/step1.go
+++ 0816/step1.go
@@ -3,9 +3,40 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 )
 
-func main() {
+var sc = bufio.NewScanner(os.Stdin)
 
+func main() {
+	var x []int
+	n := scan()
+	for i := 0; i < n; i++ {
+		x = append(x, scan())
+	}
+
+	m := make(map[int]struct{})
+	unique := make([]int, 0, len(x))
+
+	for _, v := range x {
+		if _, ok := m[v]; ok {
+			continue
+		}
+		unique = append(unique, v)
+		m[v] = struct{}{}
+	}
+
+	fmt.Println(len(unique))
+}
+
+func scan() int {
+	sc.Scan()
+	i, err := strconv.Atoi(sc.Text())
+	if err != nil {
+		os.Exit(1)
+	}
+	return i
 }
